94a1dcd0109b25b77074f6a1c7eb51c474f42dea
deekshasharma <deeksha.sharma25@gmail.com>
2020-07-21 12:43:36 -0700

    users and books in a JSON file, favorite books for a userId is WIP, added new dependencies

--- src/App.js
+++ src/App.js
@@ -4,91 +4,88 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
 export default function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/books">
-            <Books />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route exact path="/favorite/:id">
-            <MyFavorite />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/books">
+                        <Books/>
+                    </Route>
+                    <Route path="/users">
+                        <Users/>
+                    </Route>
+                    <Route exact path="/favorite/:id">
+                        <MyFavorite/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 function Home() {
-  return <h2>Welcome this is Home!</h2>;
+    return <h2>Welcome this is Home!</h2>;
 }
-
 
 
 function Books() {
-  const [collection, setCollection] = useState([]);
-  useEffect(() => {
-    getDataFromBackend("http://localhost:5000/books")
-        .then(result => {
-          const allBooks = result.bookCollection;
-          setCollection([...allBooks])
-        });
+    const [collection, setCollection] = useState([]);
+    useEffect(() => {
+        getDataFromBackend("http://localhost:5000/books")
+            .then(result => {
+                const allBooks = result.bookCollection;
+                setCollection([...allBooks])
+            });
 
-  }, []);
-  return <div>
-    {collection.map((book, key) => {
-      return <h2 key={key}>
-        {book.name} by {book.author}
-      </h2>
-    })}
-  </div>;
+    }, []);
+    return <div>
+        {collection.map((book, key) => {
+            return <h2 key={key}>
+                {book.name} by {book.author}
+            </h2>
+        })}
+    </div>;
 }
-
-
 
 
 function Users() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    getDataFromBackend("http://localhost:5000/users")
-        .then(result => setUsers([...result]));
-  }, []);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getDataFromBackend("http://localhost:5000/users")
+            .then(result => setUsers([...result]));
+    }, []);
 
-  return <div>
-  {users.map((user, key) => {
-    return<h2 key={key}>
-    {user.fName+ " "+ user.lastName}
-    </h2>
-  })}
-  </div>
+    return <div>
+        {users.map((user, key) => {
+            return <h2 key={key}>
+                {user.fName + " " + user.lastName}
+            </h2>
+        })}
+    </div>
 }
 
-function MyFavorite(){
-  const [favBooks, setFavBooks] = useState([]);
+function MyFavorite() {
+    const [favBooks, setFavBooks] = useState([]);
 
-  useEffect(() => {
-    getDataFromBackend("http://localhost:5000/favorite/f2775f38-92fc-42e5-98a5-b137a0887a40")
-        .then(result => setFavBooks(result.favoriteBooks));
-  });
+    useEffect(() => {
+        getDataFromBackend("http://localhost:5000/favorite/f2775f38-92fc-42e5-98a5-b137a0887a40")
+            .then(result => setFavBooks(result.favoriteBooks));
+    });
 
-  return <div>
-    <h1>YOUR FAVORITE BOOKS</h1>
-    {favBooks.map((book, key) => {
-      return<h3 key={key}>
-        {book.name+ " by "+ book.author}
-      </h3>
-    })}
-  </div>
+    return <div>
+        <h1>YOUR FAVORITE BOOKS</h1>
+        {favBooks.map((book, key) => {
+            return <h3 key={key}>
+                {book.name + " by " + book.author}
+            </h3>
+        })}
+    </div>
 }
 
-export const getDataFromBackend = async(endpoint) => {
-  const response = await fetch(endpoint);
-  return await response.json();
+export const getDataFromBackend = async (endpoint) => {
+    const response = await fetch(endpoint);
+    return await response.json();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
+const inventory = './database/books.json';
+const users = './database/users.json';
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 var cors = require('cors');
@@ -9,56 +14,31 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Welcome to our Home ' });
+    res.send({message: 'Welcome to our Home '});
 });
 
 app.get('/users', (req, res) => {
-    res.send([{fName: "Deeksha", lastName: "Sharma", favorite: ["1234455jeler", "7943854kejer"]}, {fName: "Amy", lastName: "Robinson", favorite: ["5467364754dfjk"]}]);
+    jsonfile.readFile(users)
+        .then(allUsers => res.send(allUsers))
+        .catch(err => console.log(err.message));
 });
 
 app.get('/books', (req, res) => {
-    res.send({bookCollection: books})
+    jsonfile.readFile(inventory)
+        .then(books => res.send({bookCollection: books}))
+        .catch(error => console.error(error));
+
 });
 
 app.get('/favorite/:id', (req, res) => {
     const userMatch = users.filter(user => user.id === req.params.id)[0];
     const favoriteBooks = [];
-        userMatch.favorite.map(bookId => {
-            const matchingBook = getMatchingBook(bookId);
-            favoriteBooks.push(matchingBook);
+    userMatch.favorite.map(bookId => {
+        const matchingBook = getMatchingBook(bookId);
+        favoriteBooks.push(matchingBook);
     });
     res.send({favoriteBooks: favoriteBooks});
 });
 
 
 const getMatchingBook = (bookId) => books.filter(book => bookId === book.id)[0];
-
-
-const books = [
-    {
-        id: "6cc12b5e-cb5e-11ea-87d0-0242ac130003",
-        name: "Surrounded by idiots",
-        author: "Thomas Erikson"
-    },
-    {
-        id: "722f584a-cb5e-11ea-87d0-0242ac130003",
-        name: "Stillness is the key",
-        author: "Ryan Holiday"
-    },
-    {
-        id: "765384e6-cb5e-11ea-87d0-0242ac130003",
-        name: "Sapiens:A Brief History of Humankind",
-        author: "Yuval Noah Harari"
-    },
-    {
-        id: "7a4fe9ea-cb5e-11ea-87d0-0242ac130003",
-        name: "Principles for Success",
-        author: "Ray Dalio"
-    },
-
-];
-
-const users = [
-    {id: "f2775f38-92fc-42e5-98a5-b137a0887a40",fName: "Deeksha", lastName: "Sharma", favorite: ["6cc12b5e-cb5e-11ea-87d0-0242ac130003", "765384e6-cb5e-11ea-87d0-0242ac130003"], role: "user"},
-    {id: "677c96e2-cb5e-11ea-87d0-0242ac130003", fName: "Amy", lastName: "Robinson", favorite: ["5467364754dfjk"], role: "admin"}
-];
