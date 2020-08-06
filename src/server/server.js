787e20cfbeb893dd9515382f91d577075b2b4918
deekshasharma <deeksha.sharma25@gmail.com>
2020-08-06 07:15:22 -0700

    Added data stores for books and users, removed cors for demo

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-app.use(cors());
 app.use(express.json());
 
 
