633fefa6c4f981999351b7fc2886d1dc7485ef85
deekshasharma <deeksha.sharma25@gmail.com>
2020-08-12 11:32:43 -0700

    Code for module03 to go in this branch

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,38 @@
 const express = require("express");
+const cors = require("cors");
+const {
+  getUserByUsername,
+  isEmptyObject,
+  isPasswordCorrect,
+} = require("./shared");
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
+app.use(cors());
 
 app.get("/users", (req, res) => {});
 
 app.get("/books", (req, res) => {});
 
-app.post("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  let base64Encoding = req.headers.authorization.split(" ")[1];
+  let credentials = Buffer.from(base64Encoding, "base64").toString().split(":");
+  const username = credentials[0];
+  const password = credentials[1];
+  getUserByUsername(username).then((user) => {
+    if (user && !isEmptyObject(user)) {
+      isPasswordCorrect(user.key, password).then((result) => {
+        result
+          ? res.status(200).send({ username: user.username, role: user.role })
+          : res
+              .status(401)
+              .send({ message: "username or password is incorrect" });
+      });
+    } else
+      res.status(401).send({ message: "username or password is incorrect" });
+  });
+});
 
 app.get("/logout", (req, res) => {});
 
