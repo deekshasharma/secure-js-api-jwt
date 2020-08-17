ab06dfd550be9a91852ac3eda907e0cef61d94f3
deekshasharma <deeksha.sharma25@gmail.com>
2020-08-17 07:44:22 -0700

    Code changes from module03 included

--- src/server/shared.js
+++ src/server/shared.js
@@ -1,5 +1,6 @@
 const jsonfile = require("jsonfile");
 const users = "./database/users.json";
+const inventory = "./database/books.json";
 const bcrypt = require("bcrypt");
 
 exports.getUserByUsername = async function (username) {
@@ -18,4 +19,40 @@ exports.isEmptyObject = (object) => Object.entries(object).length === 0;
 
 exports.isPasswordCorrect = async function (key, password) {
   return bcrypt.compare(password, key).then((result) => result);
+};
+
+exports.getAllBooks = async function () {
+  try {
+    return await jsonfile.readFile(inventory);
+  } catch (err) {
+    console.log("Error reading books: ", err);
+  }
+};
+
+exports.getAllUsers = async function () {
+  try {
+    const allUsers = await jsonfile.readFile(users);
+    let updatedUsers = [];
+    allUsers.forEach((user) => {
+      updatedUsers.push({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+      });
+    });
+    return updatedUsers;
+  } catch (err) {
+    console.log("Error reading users from datastore ", err.message);
+  }
+};
+
+exports.addBook = async function (book) {
+  try {
+    const allBooks = await jsonfile.readFile(inventory);
+    allBooks.push(book);
+    return await jsonfile.writeFile(inventory, allBooks);
+  } catch (err) {
+    return err;
+  }
 };
