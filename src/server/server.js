2e4aea87027a51db28292ae8d8c60eb3d7983e25
deekshasharma <deeksha.sharma25@gmail.com>
2020-08-20 12:14:35 -0700

    Temporary commit for bearer token

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
+require("dotenv").config({ path: "./variables.env" });
 const express = require("express");
-const cors = require("cors");
 const { uuid } = require("uuidv4");
 const {
   getUserByUsername,
@@ -8,24 +8,40 @@ const {
   getAllBooks,
   getAllUsers,
   addBook,
+  constructTokenResponse,
+  verifyToken,
+  getFavoriteBooksForUser,
+  getAudienceFromToken,
 } = require("./shared");
+const Constants = require("./constants");
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
-app.use(cors());
 
-app.get("/users", (req, res) => {
-  getAllUsers().then((users) => {
-    if (users && users.length > 0) res.status(200).send({ users: users });
-    else res.status(500).send({ users: [] });
-  });
+app.get("/users", verifyToken, (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (getAudienceFromToken(token).includes(Constants.SHOW_USERS)) {
+    getAllUsers().then((users) => {
+      if (users && users.length > 0) {
+        constructTokenResponse(token, null).then((token) => {
+          res.set("Authorization", "Bearer Token " + token);
+          res.status(200).send({ users: users });
+        });
+      } else res.status(500).send({ users: [] });
+    });
+  } else res.status(403).send({ message: "Not authorized to view users" });
 });
 
-app.get("/books", (req, res) => {
+app.get("/books", verifyToken, (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
   getAllBooks().then((books) => {
-    if (books && books.length > 0) res.status(200).send({ books: books });
-    else res.status(500).send({ books: [] });
+    if (books && books.length > 0) {
+      constructTokenResponse(token, null).then((token) => {
+        res.set("Authorization", "Bearer Token " + token);
+        res.status(200).send({ books: books });
+      });
+    } else res.status(500).send({ books: [] });
   });
 });
 
@@ -37,26 +53,49 @@ app.post("/login", (req, res) => {
   getUserByUsername(username).then((user) => {
     if (user && !isEmptyObject(user)) {
       isPasswordCorrect(user.key, password).then((result) => {
-        result
-          ? res.status(200).send({ username: user.username, role: user.role })
-          : res
-              .status(401)
-              .send({ message: "username or password is incorrect" });
+        if (!result)
+          res
+            .status(401)
+            .send({ message: "username or password is incorrect" });
+        else {
+          constructTokenResponse(null, username).then((token) => {
+            res.set("Authorization", "Bearer Token " + token);
+            res.status(200).send({ username: user.username, role: user.role });
+          });
+        }
       });
     } else
       res.status(401).send({ message: "username or password is incorrect" });
   });
 });
 
-app.get("/logout", (req, res) => {});
+app.get("/logout", verifyToken, (req, res) => {
+  res.status(200).send({ message: "Signed out" });
+});
 
-app.get("/favorite", (req, res) => {});
+app.get("/favorite", verifyToken, (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  getFavoriteBooksForUser(token).then((books) => {
+    constructTokenResponse(token, null).then((token) => {
+      res.set("Authorization", "Bearer Token " + token);
+      res.status(200).send({ favorites: books });
+    });
+  });
+});
 
-app.post("/book", (req, res) => {
-  addBook({ name: req.body.name, author: req.body.author, id: uuid() }).then(
-    (err) => {
-      if (err) res.status(500).send({ message: "Cannot add this book" });
-      else res.status(200).send({ message: "Book added successfully" });
-    }
-  );
+app.post("/book", verifyToken, (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (getAudienceFromToken(token).includes(Constants.ADD_BOOK)) {
+    addBook({ name: req.body.name, author: req.body.author, id: uuid() }).then(
+      (err) => {
+        if (err) res.status(500).send({ message: "Cannot add this book" });
+        else {
+          constructTokenResponse(token, null).then((token) => {
+            res.set("Authorization", "Bearer Token " + token);
+            res.status(200).send({ message: "Book added successfully" });
+          });
+        }
+      }
+    );
+  } else res.status(403).send({ message: "Not authorized to add a book" });
 });
--- src/server/shared.js
+++ src/server/shared.js
@@ -2,8 +2,10 @@ const jsonfile = require("jsonfile");
 const users = "./database/users.json";
 const inventory = "./database/books.json";
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Constants = require("./constants");
 
-exports.getUserByUsername = async function (username) {
+var getUserByUsername = (exports.getUserByUsername = async function (username) {
   try {
     const allUsers = await jsonfile.readFile(users);
     const filteredUserArray = allUsers.filter(
@@ -13,7 +15,7 @@ exports.getUserByUsername = async function (username) {
   } catch (err) {
     console.log("Error reading users: ", err.message);
   }
-};
+});
 
 exports.isEmptyObject = (object) => Object.entries(object).length === 0;
 
@@ -55,4 +57,56 @@ exports.addBook = async function (book) {
   } catch (err) {
     return err;
   }
+};
+
+const getUsernameFromToken = (token) => jwt.decode(token)["sub"];
+exports.getAudienceFromToken = (token) => jwt.decode(token)["aud"];
+
+const generateToken = (username, role) => {
+  const payload = {};
+  const options = {
+    algorithm: process.env.ALGORITHM,
+    expiresIn: process.env.EXPIRY,
+    issuer: process.env.ISSUER,
+    subject: username,
+    audience:
+      role === "admin"
+        ? Constants.JWT_OPTIONS.ADMIN_AUDIENCE
+        : Constants.JWT_OPTIONS.MEMBER_AUDIENCE,
+  };
+  return jwt.sign(payload, process.env.SECRET, options);
+};
+
+exports.constructTokenResponse = async function (token, userName) {
+  let name = userName || getUsernameFromToken(token);
+  const user = await getUserByUsername(name);
+  return generateToken(user.username, user.role);
+};
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token)
+    res.status(401).send({ message: "Not Authorized to access data" });
+  else {
+    jwt.verify(token, process.env.SECRET, function (err) {
+      if (err) {
+        res
+          .status(401)
+          .send({ message: "Please login again! Your session has expired" });
+      } else next();
+    });
+  }
+};
+
+exports.getFavoriteBooksForUser = async function (token) {
+  const username = getUsernameFromToken(token);
+  const user = await getUserByUsername(username);
+  const favoriteBookIds = user["favorite"];
+  const favoriteBooks = [];
+  if (favoriteBookIds.length === 0) return favoriteBooks;
+  const allBooks = await jsonfile.readFile(inventory);
+  favoriteBookIds.forEach((id) =>
+    favoriteBooks.push(allBooks.filter((book) => id === book.id)[0])
+  );
+  return favoriteBooks;
 };
