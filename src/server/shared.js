6245d1e7d13fe9fa26f415671050356a6ff590d8
deekshasharma <deeksha.sharma25@gmail.com>
2020-08-25 12:47:12 -0700

    Check for no auth header

--- src/server/shared.js
+++ src/server/shared.js
@@ -80,15 +80,19 @@ exports.generateToken = async function (prevToken, userName) {
 };
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  if (!token)
-    res.status(401).send({ message: "Not Authorized to access data" });
+  if (!req.headers.authorization)
+    res.status(401).send({ message: "Not authorized to access data" });
   else {
-    jwt.verify(token, process.env.SECRET, function (err) {
-      if (err) {
-        res.status(401).send({ message: "Please login again" });
-      } else next();
-    });
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token)
+      res.status(401).send({ message: "Not Authorized to access data" });
+    else {
+      jwt.verify(token, process.env.SECRET, function (err) {
+        if (err) {
+          res.status(401).send({ message: "Please login again" });
+        } else next();
+      });
+    }
   }
 };
 
